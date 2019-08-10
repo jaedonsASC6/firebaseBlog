570a5f8b531e2dfddc5be8373df31190fdcb3836
jaedonsASC6 <jsatchell2017@gmail.com>
2019-08-10 00:39:27 -0400

    Added a check to see if blog entry fields were empty

--- createBlog.js
+++ createBlog.js
@@ -6,20 +6,29 @@ submitButton.addEventListener("click",updateDB);
 
 function updateDB(event){
     event.preventDefault();
+
     const title        = blogTitleElement.value;
     const blogContent  = blogContentElement.value;
+    
+    if(title == "" && blogContent == ""){
+        alert("Please Enter A Title And Content For Your Post")
+    }else if(title == ""){
+        alert("Please Enter A title For your Blog")
+    }else if (blogContent == ""){
+        alert("Please Enter Content For Your Blog")
+    }else{
+        blogTitleElement.value = "";
+        blogContentElement.value  = "";
 
-    blogTitleElement.value = "";
-    blogContentElement.value  = "";
+        console.log(title + " : " + blogContent);
 
-    console.log(title + " : " + blogContent);
+        //Update database here
 
-    //Update database here
-
-    const blogObj = {
-        TITLE: title,
-        BLOGCONTENT: blogContent
+        const blogObj = {
+            TITLE: title,
+            BLOGCONTENT: blogContent
+        }
+        database.push(blogObj)
+        alert("Blog Posted")
     }
-    database.push(blogObj)
-    alert("Blog Posted")
 }
